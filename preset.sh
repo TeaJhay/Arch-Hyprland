495569c90f6d3915c9d909bea1cd42946b4011ee
TeaJhay <140999577+TeaJhay@users.noreply.github.com>
2024-12-20 18:53:25 +1000

    Update preset.sh

--- preset.sh
+++ preset.sh
@@ -11,7 +11,7 @@
 aur_helper="yay"
 ############ use : "Y" or "N"
 ###-Do you have any nvidia gpu in your system?
-nvidia="Y"
+nvidia="N"
 ###-Install GTK themes (required for Dark/Light function)?
 gtk_themes="Y"
 ###-Do you want to configure Bluetooth?
@@ -19,7 +19,7 @@ bluetooth="Y"
 ###-Do you want to install Thunar file manager?
 thunar="Y"
 ###-Install & configure SDDM log-in Manager plus (OPTIONAL) SDDM Theme?
-sddm="Y"
+sddm="N"
 ###-Install XDG-DESKTOP-PORTAL-HYPRLAND? (For proper Screen Share ie OBS)
 xdph="Y"
 ###-Install zsh, oh-my-zsh & (Optional) pokemon-colorscripts?
@@ -34,16 +34,16 @@ dots="Y"
 blacklist_nouveau="Y"
 # Ask the user if they want to use Thunar as the default file manager
 # Do you want to set Thunar as the default file manager? (y/n): "
-thunar_default="Y"
+thunar_default="N"
 ### XDG-desktop-portal-KDE & GNOME (if installed) should be manually disabled or removed! Script cant remove nor disable it.
 ### Would you like to try to remove other XDG-Desktop-Portal-Implementations? (y/n) 
-XDPH1="Y"
+XDPH1="N"
 ### OPTIONAL - Would you like to install SDDM themes? (y/n)
 install_sddm_theme="Y"
 ### " This script will add your user to the 'input' group."
 ### " Please note that adding yourself to the 'input' group might be necessary for waybar keyboard-state functionality."
 input_group_choid="Y"
 ### OPTIONAL - Do you want to add Pokemon color scripts? (y/n): 
-pokemon_choice="Y"
+pokemon_choice="N"
 ### Do you want to upgrade to the latest version? (y/n) - This is for the dotfiles
 upgrade_choice="Y"
